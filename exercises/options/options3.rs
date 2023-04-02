162f9e13fdf2465ed9df9ad18aa19e0ae751f8e8
MIracleyin <miracleyin@live.com>
2023-04-02 19:24:36 +0800

    options

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,7 +1,6 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -12,7 +11,8 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // ref 和 & 在使用时的上下文和含义是不同的。ref 用于模式匹配和变量绑定，而 & 用于创建变量的引用。
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
